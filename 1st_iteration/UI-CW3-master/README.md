a783169f19f9383828dda59fa6b557f01d86daf8
GoatXYX <93130896+GoatXYX@users.noreply.github.com>
2022-12-19 18:56:09 +0800

    The first iteration. Functions added to tomeo.

---
+++ 1st_iteration/UI-CW3-master/README.md
@@ -0,0 +1,7 @@
+## COMP 2811 - tomeo video player - coursework 3
+
+Group members:
+Hanna Suleman (sc19hs)
+Thomas Killeen (sc18twk)
+Eliza Margaritescu (sc19em)
+Olivia Broekhuis (sc19ob)
---
+++ 1st_iteration/UI-CW3-master/the_player.cpp
@@ -0,0 +1,30 @@
+//
+// Created by twak on 11/11/2019.
+//
+
+#include "the_player.h"
+#include "application.h"
+
+using namespace std;
+
+// all buttons have been setup, store pointers here
+void ThePlayer::setContent(std::vector<TheButton*>* b, std::vector<TheButtonInfo>* i) {
+    buttons = b;
+    infos = i;
+    jumpTo(buttons -> at(0) -> info);
+}
+
+
+void ThePlayer::playStateChanged (QMediaPlayer::State ms) {
+    switch (ms) {
+        case QMediaPlayer::State::StoppedState:
+            play(); // starting playing again...
+            break;
+    default:
+        break;
+    }
+}
+
+void ThePlayer::jumpTo (TheButtonInfo* button) {
+    setMedia( * button -> url); // gives filepath of media to play
+}
